91b4d21ac48bccee537c82e88c81c5e860622674
D-E-F-E-A-T <kushchoudhary8@gmail.com>
2020-04-27 20:21:07 +0530

    bug resolved

--- fastProxy/__init__.py
+++ fastProxy/__init__.py
@@ -1,1 +1,6 @@
-from fastProxy import *
+from .fastProxy import (THREAD_COUNT,
+                       REQUEST_TIMEOUT,
+                       GENERATE_CSV,
+                       ALL_IPS,
+                       printer,
+                       fetch_proxies)
